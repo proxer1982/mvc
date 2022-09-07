c5fd66d0c12c052024bba72ad11a42069f93482e
proxer1982 <juanc.zorro@gmail.com>
2022-09-06 22:32:31 -0500

    iniciando

---
+++ src/main/java/ciclo3/mvc/controller/IndexController.java
@@ -0,0 +1,13 @@
+package ciclo3.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        System.out.println(IndexController.class + " - Index");
+        return "index";
+    }
+}
