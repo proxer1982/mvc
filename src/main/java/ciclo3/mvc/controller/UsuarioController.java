db3cea4a89c9a38c2ef68be8c18ebf17dcc17998
proxer1982 <juanc.zorro@gmail.com>
2022-09-08 22:40:00 -0500

    clase 3

--- src/main/java/ciclo3/mvc/controller/UsuarioController.java
+++ src/main/java/ciclo3/mvc/controller/UsuarioController.java
@@ -1,11 +1,16 @@
 package ciclo3.mvc.controller;
 
+import ciclo3.mvc.entities.Rol;
+import ciclo3.mvc.entities.TipoDocumento;
 import ciclo3.mvc.entities.Usuario;
+import ciclo3.mvc.service.IRolService;
+import ciclo3.mvc.service.ITipoDocumentoService;
 import ciclo3.mvc.service.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.logging.Level;
@@ -15,6 +20,10 @@ import java.util.logging.Logger;
 public class UsuarioController {
     @Autowired
     private IUsuarioService usuarioService;
+    @Autowired
+    private IRolService rolService;
+    @Autowired
+    private ITipoDocumentoService tipoDocumentoService;
     private final Logger log = Logger.getLogger(""+UsuarioController.class);
 
     @GetMapping("/usuarios/list")
@@ -26,5 +35,24 @@ public class UsuarioController {
         model.addAttribute("mensaje",mensaje);
         model.addAttribute("usuarios",usuarios);
         return "usuarios/list";
+    }
+
+    @GetMapping("/usuarios/modificar")
+    public String createUsuario(Model model){
+        log.log(Level.INFO, "modificarUsuarios");
+        Usuario usuario = new Usuario();
+        List<TipoDocumento> tipoDoc = tipoDocumentoService.findAll();
+        List<Rol> roles = rolService.findAll();
+        model.addAttribute("usuario",usuario);
+        model.addAttribute("roles",roles);
+        model.addAttribute("tipodoc",tipoDoc);
+        return "usuarios/modificar";
+    }
+
+    @PostMapping("/usuarios/guardar")
+    public String saveUsuario(Model model){
+        log.log(Level.INFO, "saveUsuarios");
+
+        return "usuarios/modificar";
     }
 }
